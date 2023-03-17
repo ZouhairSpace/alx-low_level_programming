7e9d1f83d0690f111e4d9b9047d3a5226c4076ab
ZouhairSpace <jeddi .zouhair@gmail.com>
2023-03-16 17:27:50 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description : A c programm that prints with put function 
- * 
- * Return : Always 0 (Success)
+ * Description: A c programm that prints with put function
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
