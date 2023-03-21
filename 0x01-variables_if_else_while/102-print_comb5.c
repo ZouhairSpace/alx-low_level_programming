d301dc5f21bdcebd0daa4b0782a7218529e43a96
ZouhairSpace <jeddi .zouhair@gmail.com>
2023-03-20 18:31:25 -0700

    Task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	int i; 
+	int i;
 	int j;
 	int k;
 	int h;
@@ -20,19 +20,23 @@ int main(void)
 	{
 		for (j = 48; j <= 56; j++)
 		{
-		       for (k = 48; k <= 57 ; k++)
-		       {
+			for (k = 48; k <= 57 ; k++)
+			{
 				for (h = 49; h <= 57; h++)
 				{
-			 		if (h+k <= i+j || k<i)
+					if (h + k <= i + j || k < i)
+					{
 						continue;
+					}
 					putchar(i);
 					putchar(j);
 					putchar(32);
 					putchar(k);
 					putchar(h);
-					if (i = 57 && j = 56 && k = 57 && h = 57)
+					if (i == 57 && j == 56 && k == 57 && h == 57)
+					{
 						continue;
+					}
 					putchar(44);
 					putchar(32);
 				}
@@ -41,4 +45,5 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
+
