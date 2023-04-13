637e644980ed2d216ee0162cf9164ed753d18bf3
ZouhairSpace <jeddi .zouhair@gmail.com>
2023-04-12 19:21:26 -0700

    t0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,20 +19,17 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	else
-	{
-		ptr = malloc(size * sizeof(char));
 
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			for (i = 0; i < size; i++)
-			{
-				ptr[i] = c;
-			}
-			return (ptr);
-		}
+	ptr = malloc(sizeof(c) * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
 }
