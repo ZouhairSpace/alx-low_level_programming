99e3324748212527128c1dbcc3cf1edddd0219e3
ZouhairSpace <jeddi .zouhair@gmail.com>
2023-05-12 17:13:45 -0700

    t0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int bytes_read, bytes_written;
+	char *buffer;
+	
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+	close(fd);
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	return (bytes_written);
+}
